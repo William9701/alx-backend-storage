42b4ae460386dac085bed1615ae5fb89c97fe5db
Obi William <williamobi818@gmail.com>
2024-01-18 12:37:03 +0100

    we do hard things by christ who strenghtens us

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,5 +1,4 @@
--- this sql counts the unique fans of a particullar band
-SELECT origin, COUNT(fans) AS nb_fans
+SELECT origin, SUM(fans) AS nb_fans
 FROM metal_bands
 GROUP BY origin
 ORDER BY nb_fans DESC;
--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,5 +1,4 @@
--- This sql lists all bands with Glam rock as their main style, ranked by their longevity
 SELECT band_name, (2022 - formed) AS lifespan
 FROM metal_bands
-WHERE style = 'Glam rock' AND split IS NOT NULL
+WHERE style LIKE '%Glam rock%'
 ORDER BY lifespan DESC;
