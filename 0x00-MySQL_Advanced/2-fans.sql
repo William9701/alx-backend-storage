4cd35505bcfe05e7a8c75843b28af5371ef04030
Obi William <williamobi818@gmail.com>
2024-01-18 12:38:44 +0100

    we do hard things by christ who strenghtens us

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,3 +1,4 @@
+-- This querry sums up all the fans
 SELECT origin, SUM(fans) AS nb_fans
 FROM metal_bands
 GROUP BY origin
--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,3 +1,4 @@
+-- this method retuns the logitivity
 SELECT band_name, (2022 - formed) AS lifespan
 FROM metal_bands
 WHERE style LIKE '%Glam rock%'
