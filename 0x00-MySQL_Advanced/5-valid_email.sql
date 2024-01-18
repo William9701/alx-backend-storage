70683dd3ea3584017d7ccd621fe32d63dac70772
Obi William <williamobi818@gmail.com>
2024-01-18 21:30:42 +0100

    we do hard things by christ who strenghtens us

--- 0x00-MySQL_Advanced/5-valid_email.sql
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -1,7 +1,7 @@
 -- this is a triger that changes the valid email status to 0
 DELIMITER $$
 CREATE TRIGGER reset_valid_email
-AFTER UPDATE ON users
+BEFORE UPDATE ON users
 FOR EACH ROW
 BEGIN
    IF OLD.email != NEW.email THEN
