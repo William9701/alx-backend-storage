59f6983dcd42fb6a552c86d04a5ffbbb0130225b
Obi William <williamobi818@gmail.com>
2024-01-23 22:52:36 +0100

    this is a nosql beginers tutorials updated

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -1,25 +1,26 @@
 #!/usr/bin/env python3
-"""This is a mangodb tutorial module"""
+"""
+ Python script that provides some stats about Nginx logs stored in MongoDB:
+"""
 
 from pymongo import MongoClient
 
-
-def log_stats(mongo_collection):
-    """ Provides some stats about Nginx logs stored in MongoDB """
-    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-    print(f"{mongo_collection.count_documents({}):} logs")
-
-    print("Methods:")
-    for method in methods:
-        count = mongo_collection.count_documents({"method": method})
-        print(f"\tmethod {method}: {count}")
-
-    status_check_count = mongo_collection.count_documents(
-        {"method": "GET", "path": "/status"})
-    print(f"{status_check_count} status check")
-
-
 if __name__ == "__main__":
+    """Display the statistics about Nginx logs stored in MongoDB."""
     client = MongoClient('mongodb://127.0.0.1:27017')
-    logs_collection = client.logs.nginx
-    log_stats(logs_collection)
+    nginx_collection = client.logs.nginx
+
+    n_logs = nginx_collection.count_documents({})
+    print(f'{n_logs} logs')
+
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    print('Methods:')
+    for method in methods:
+        count = nginx_collection.count_documents({"method": method})
+        print(f'\tmethod {method}: {count}')
+
+    status_check = nginx_collection.count_documents(
+        {"method": "GET", "path": "/status"}
+    )
+
+    print(f'{status_check} status check')
--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -7,4 +7,3 @@ def insert_school(mongo_collection, **kwargs):
     document = kwargs
     result = mongo_collection.insert_one(document)
     return result.inserted_id
-
